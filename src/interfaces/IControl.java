7df1dc637e753d260df5414ace07bede694e05a0
Doug Reimer <nicad49@gmail.com>
2016-07-12 23:03:28 -0600

    first commit

---
+++ src/interfaces/IControl.java
@@ -0,0 +1,6 @@
+package interfaces;
+
+public interface IControl {
+	void getId();
+	void getLayout();
+}
---
+++ src/models/ExerciseControl.java
@@ -0,0 +1,19 @@
+package models;
+
+import interfaces.IControl;
+
+public class ExerciseControl implements IControl {
+
+	@Override
+	public void getId() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void getLayout() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
